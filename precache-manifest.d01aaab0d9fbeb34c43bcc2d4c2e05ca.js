a0fb997ba9ee75dfb3eeba4148eb697081e68012
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2020-12-28 13:11:23 -0500

    Updates

---
+++ precache-manifest.d01aaab0d9fbeb34c43bcc2d4c2e05ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3c2ffbd7a9a5eec9863f47626c3f5a",
+    "url": "/Vinay-Shoppies/index.html"
+  },
+  {
+    "revision": "c82b2b553175ee110285",
+    "url": "/Vinay-Shoppies/static/js/2.856f87d0.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/Vinay-Shoppies/static/js/2.856f87d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a6d0d24051588b5fb2",
+    "url": "/Vinay-Shoppies/static/js/main.ddcb12cd.chunk.js"
+  },
+  {
+    "revision": "2e3977b86b83cfebef1d",
+    "url": "/Vinay-Shoppies/static/js/runtime-main.c6fab60f.js"
+  },
+  {
+    "revision": "104a1a91ed03ecbac6e5ce17e130cf15",
+    "url": "/Vinay-Shoppies/static/media/noImage.104a1a91.png"
+  }
+]);
